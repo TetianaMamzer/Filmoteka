c04f053a384d3e92974f91a35d93d06d768a424e
TetianaMamzer <tetianamamzer@gmail.com>
2023-01-25 15:04:35 +0200

    +

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './js/preloader';
+
+import './js/trending-films-render';
+
+
+
+import './js/modal-film';
+
+
+import './js/film-modal-render';
+
+import './js/search-films';
+
+import './js/open-md-win-our-team';
+import './js/slider';
