405ec6cfcbaae1d7427b08f098b64bad173aa5f3
TetianaMamzer <tetianamamzer@gmail.com>
2023-02-05 22:11:32 +0200

    add-img

--- src/js/swiper-slide.js
+++ src/js/swiper-slide.js
@@ -4,7 +4,10 @@ import {
 
 const refs = {
   swiper: document.querySelector('.swiper-wrapper'),
-  shadow: document.querySelector('.swiper-shadow')
+  shadow: document.querySelector('.swiper-shadow'),
+  sliderMain: document.querySelector('.slider_main'),
+  sliderBg: document.querySelector('.slider_bg'),
+  sliderItems: document.querySelectorAll('.slider__item')
 
 }
 
@@ -33,4 +36,42 @@ function createPopularList(data) {
   .join('');
 }
 
+const sliderMain = new Swiper(refs.sliderMain, {
+  freeMode: true,
+  centeredSlides: true,
+  mousewheel: true,
+  parallax: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2.5,
+      spaceBetween: 20
+    },
+    680: {
+      slidesPerView: 3.5,
+      spaceBetween: 60
+    }
+  }
 
+})
+
+const sliderBg = new Swiper(refs.sliderBg, {
+  centeredSlides: true,
+  parallax: true,
+  spaceBetween: 60,
+  slidesPerView: 3.5
+
+})
+
+sliderMain.controller.control = sliderBg
+
+refs.sliderItems.forEach(item => {
+  item.addEventListener('click', e => {
+    console.log(e)
+    item.classList.toggle('opened')
+  })
+})
+
+let desc = document.querySelector('.description')
+sliderMain.on('slideChange', e => {
+  sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
+})
